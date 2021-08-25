28fe9926e2a3fe4d4eda2e4448203eb561e99bc0
basuabhirup <basu.abhirup@yahoo.com>
2021-08-25 19:31:43 +0530

    Create Work List using the same EJS template.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 
 let items = ['Buy Food', 'Cook Food', 'Eat Food'];
+let workItems = ['Watch JavaScript Tutorial', 'Design a jewelery website'];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,13 +19,22 @@ app.get("/", (req, res) => {
   }
   let today = new Date();
   let day = today.toLocaleDateString('en-US', options);
-  res.render("list", {day: day, items: items});
+  res.render("list", {title: day, items: items});
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", {title: "Work List", items: workItems});
 });
 
 app.post("/", (req,res) => {
   let item = req.body.item;
-  items.push(item);
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }  
 })
 
 app.listen(port, () => {
